f96caf4e5f2de4c7a867b4f0948966a24eae4795
baiziyu-fe <baiziyu-fe@outlook.com>
2021-03-05 23:25:01 +0800

    fix: 修复Vite模式打包失败的问题

--- template/vite-ts/vite.config.ts
+++ template/vite-ts/vite.config.ts
@@ -5,15 +5,13 @@ import prodConfig from './config/prod';
 
 export default ({ command, mode }) => {
   let config = {};
-  if (command === 'serve') {
-    switch (mode) {
-      case 'production':
-        config = prodConfig;
-        break;
-      default:
-        config = devConfig;
-        break;
-    };
-  }
+  switch (mode) {
+    case 'production':
+      config = prodConfig;
+      break;
+    default:
+      config = devConfig;
+      break;
+  };
   return defineConfig(config);
 };
