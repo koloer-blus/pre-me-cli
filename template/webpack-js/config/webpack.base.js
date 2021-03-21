c6ec97bd457628eb8daec2f8d42784f919db8f81
baiziyu-fe <77950278+baiziyu-fe@users.noreply.github.com>
2021-03-22 00:11:16 +0800

    fix: 修复webpack模板下出现hash缓存无法命中的情况

--- template/webpack-js/config/webpack.base.js
+++ template/webpack-js/config/webpack.base.js
@@ -160,6 +160,16 @@ module.exports = merge(webpackConfig, {
     // 清理打包目录
     new CleanWebpackPlugin(),
     new HardSourceWebpackPlugin(),
+    new HardSourceWebpackPlugin.ExcludeModulePlugin([
+      {
+        // HardSource works with mini-css-extract-plugin but due to how
+        // mini-css emits assets, assets are not emitted on repeated builds with
+        // mini-css and hard-source together. Ignoring the mini-css loader
+        // modules, but not the other css loader modules, excludes the modules
+        // that mini-css needs rebuilt to output assets every time.
+        test: /mini-css-extract-plugin[\\/]dist[\\/]loader/,
+      },
+    ]),
     new HtmlWebpackPlugin({
       inject: true,
       template: config.indexPath,
