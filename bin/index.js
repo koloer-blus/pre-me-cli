92c76d6432a024ca516a93854859412a706f7dab
baiziyu-fe <baiziyu-fe@outlook.com>
2022-07-05 12:25:19 +0800

    fix(remove): remove useless file

--- bin/index.js
+++ bin/index.js
@@ -1,12 +1,6 @@
 #!/usr/bin/env node
 
-const updateNotifier = require('update-notifier');
-const ora = require('ora');
-const chalk = require('chalk');
-const symbols = require('log-symbols');
-const inquirer = require('inquirer');
-const handlebars = require('handlebars');
-const fs = require('fs-extra');
+
 const path = require('path');
 const { Command, Option } = require('commander');
 const pkg = require('../package.json');
@@ -17,25 +11,23 @@ const { version, name } = pkg;
 const program = new Command();
 // ==>version
 
-const templatePath = path.resolve(__dirname, `../template/`);
-const aimPath = process.cwd();
 
 program
-  .version(version, '-v, --version', `Output your local ${name}'s version.`);
+    .version(version, '-v, --version', `Output your local ${name}'s version.`);
 
 // init
 program
-  .name('pre-me-cli')
-  .usage('<commands> [options]')
-	.command('init [fileDirName]')
-  .action((fileDirName = 'template') => {
-    const dirName = fileDirName.toLowerCase();
-    if (/^[a-zA-Z][a-zA-Z0-9\-_]{2,38}$/.test(dirName)) {
-      copyTemplate(dirName);
-    } else {
-      message.print(1, message.init.error, `The name「「${dirName}」」 of the project does not conform to the specification.`);
-    }
-  });
+    .name('pre-me-cli')
+    .usage('<commands> [options]')
+    .command('init [fileDirName]')
+    .action((fileDirName = 'template') => {
+        const dirName = fileDirName.toLowerCase();
+        if (/^[a-zA-Z][a-zA-Z0-9\-_]{2,38}$/.test(dirName)) {
+            copyTemplate(dirName);
+        } else {
+            message.print(1, message.init.error, `The name「「${dirName}」」 of the project does not conform to the specification.`);
+        }
+    });
 
 program.parse(program.argv);
 
